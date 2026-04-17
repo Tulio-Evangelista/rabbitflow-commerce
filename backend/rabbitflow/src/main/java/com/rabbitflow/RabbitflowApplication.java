08b493f8d7fba0c9f3f2e9420f1407330ad99899
SiroiluT <tulio-evangelista@outlook.com>
2026-04-16 23:28:43 -0300

    Initial commit - RabbitFlow Commerce project structure

---
+++ backend/rabbitflow/src/main/java/com/rabbitflow/RabbitflowApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitflowApplication.class, args);
+	}
+
+}
